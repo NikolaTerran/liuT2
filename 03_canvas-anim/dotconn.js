319593b34512dae80e2330f91c472c7723b4918a
terran <tliu3@stuy.edu>
2019-02-05 21:40:38 -0500

    wierd

--- 03_canvas-anim/dotconn.js
+++ 03_canvas-anim/dotconn.js
@@ -1,5 +1,17 @@
+//Tianrun Liu
+
+//SoftDev2 pd6
+
+//K #03: They lock us in the tower whenever we get caught ...which is often
+
+//2019-02-04    
+
+
 //jmai3
 //Jiajie mai
+
+//global state variable
+var color = "#000000";
 
 // drawing state var
 var dsize = 5;
@@ -12,10 +24,24 @@ var oldx, oldy;
 var newx, newy;
 
 // anime state var
-var start_time;
-var radius;
+var start_time = null;
+var radius = 0.1;
+var ani_state = 0;
 
-//element selections
+//general selections
+var red = document.getElementById("red");
+var orange = document.getElementById("orange");
+var yellow = document.getElementById("yellow");
+var green = document.getElementById("green");
+var cyan = document.getElementById("cyan");
+var blue = document.getElementById("blue");
+var purple = document.getElementById("purple");
+var black = document.getElementById("black");
+var white = document.getElementById("white");
+//var red = document.getElementById("red");
+
+
+//draw element selections
 var c = document.getElementById("playground");
 var la = document.getElementById("clear");
 var ctx = c.getContext("2d");
@@ -38,8 +64,10 @@ var rget = document.getElementById("rectsize");
 var youtube = document.getElementById("prevent");
 var toggle = document.getElementById("switch");
 
+
+//anime element selection
 var anime = document.getElementById("anime");
-var start = document.getElementById("ani_start");
+var starto = document.getElementById("ani_start");
 var stop = document.getElementById("stop");
 
 var ani_con = anime.getContext("2d");
@@ -50,8 +78,8 @@ toggle.addEventListener("click", rect_dot);
 c.addEventListener("click", draw);
 la.addEventListener("click",clear);
 
-//d.addEventListener("click",changed);
-//r.addEventListener("click",changer);
+	//d.addEventListener("click",changed);
+	//r.addEventListener("click",changer);
 
 di.addEventListener("click",dsizeu);
 dd.addEventListener("click",dsized);
@@ -62,7 +90,26 @@ wd.addEventListener("click",wsized);
 hi.addEventListener("click",hsizeu);
 hd.addEventListener("click",hsized);
 
-start.addEventListener("click",draw_circle);
+starto.addEventListener("click",draw_circle);
+stop.addEventListener("click", stop_ani);
+
+
+
+//https://stackoverflow.com/questions/256754/how-to-pass-arguments-to-addeventlistener-listener-function
+red.addEventListener("click",function(){poison(1);});
+orange.addEventListener("click",function(){poison(2);});
+yellow.addEventListener("click",function(){poison(3);});
+green.addEventListener("click",function(){poison(4);});
+cyan.addEventListener("click",function(){poison(5);});
+blue.addEventListener("click",function(){poison(6);});
+purple.addEventListener("click",function(){poison(7);});
+black.addEventListener("click",function(){poison(8);});
+white.addEventListener("click",function(){poison(9);});
+
+
+
+
+
 //click this link won't take you to youtube like default
 youtube.addEventListener("click",function(event){event.preventDefault();
 						 clear();});
@@ -72,15 +119,77 @@ youtube.addEventListener("click",function(event){event.preventDefault();
 //anime stuff
 	
 //
-function draw_circle(timestamp){
-
-	ani_con.beginPath();
-	ani_con.arc(anime.width/2,anime.height/2,radius,0,2 * Math.PI);
-	ani_con.fill();
+function foo(){
+	console.log("foo");
 }
 
-function stop(e){}
-window.requestAnimationFrame(draw_circle);
+var progress;
+var id;
+
+function poison(x){
+	switch(x){
+		case 1: color = "#FF0000";
+		break; 	
+		case 2: color = "#FFA500";
+		break;
+		case 3: color = "#FFFF00";
+		break;
+		case 4: color = "#008000";
+		break;
+		case 5: color = "#00FFFF";
+		break;
+		case 6: color = "#0000FF";
+		break;
+		case 7: color = "#800080";
+		break;
+		case 8: color = "#000000";
+		break;
+		case 9: color = "#FFFFFF";
+		break;
+		case 10:
+		break;
+		case 11:
+		break;
+	}
+	return x;
+}
+
+function draw_circle(timestamp){
+	console.log(timestamp);
+	if(!start_time){
+		start_time = timestamp;
+	}
+	progress = timestamp - start_time;
+	ani_con.clearRect(0,0,anime.height,anime.width);
+	ani_con.beginPath();
+	try{
+		ani_con.arc(anime.height/2,anime.width/2, radius , 0 , 2 * Math.PI);
+		ani_con.stroke();
+		ani_con.fillStyle = color;
+		ani_con.fill();
+	}catch{}
+	if(radius >= (anime.width/2)){
+		ani_state = 1;
+		//radius += 0.1;
+	}
+	
+	if(radius < 0){
+		ani_state = 0;
+	}
+	
+	if(ani_state == 1){
+		radius -= 0.5;
+	}else{
+		radius += 0.5;
+	}
+	
+	   id = requestAnimationFrame(draw_circle);
+}
+
+function stop_ani(){
+	cancelAnimationFrame(id);
+}
+
 
 //////////////////////////////////////////////////////
 function rect_dot(e){
@@ -139,8 +248,9 @@ function draw(e){
 	subtoggle == 0){
 		//began tracing the circle
 		ctx.beginPath();
-ctx.arc(x, y, dsize, 0, 2 * Math.PI);
-ctx.fill();
+		ctx.arc(x, y, dsize, 0, 2 * Math.PI);
+		ctx.fillStyle = color;
+		ctx.fill();
 		if(typeof oldx == 'undefined'){
 			oldx = x;
 			oldy = y;
@@ -151,6 +261,7 @@ ctx.fill();
 			newy = y;
 			ctx.moveTo(oldx,oldy);
 			ctx.lineTo(newx,newy);
+			ctx.strokeStyle = color;
 			ctx.stroke();
 			oldx = x;
 			oldy = y;
