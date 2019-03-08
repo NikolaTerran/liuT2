3029d0088eb4d25301e4d0efbbfb0273b7775482
Brian Lee <hlee5@stuy.edu>
2019-03-07 22:33:30 -0500

    create db

--- 08_mongosite/app.py
+++ 08_mongosite/app.py
@@ -9,11 +9,10 @@ https://www.govtrack.us/api/v2/role?current=true&role_type=senator
 """
 
 import os
-import pprint
+import json
 
 from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
-from bson import json_util
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
@@ -38,6 +37,8 @@ def main():
 @app.route('/config', methods=['GET','POST'])
 def config():
 	if request.method == 'POST':
+		destroy_db(session['addr'])
+
 		addr = request.form['ip']
 		if len(addr) == 0:
 			addr = DEFAULTADDR
@@ -46,6 +47,10 @@ def config():
 		db = client[DBNAME]
 		collection = db[COLNAME]
 		session['addr'] = addr
+
+		# Probably should handle errors
+		create_db(addr)
+			
 		return render_template("config.html", ip=addr)
 	else:
 		if 'addr' not in session:
@@ -70,20 +75,20 @@ def respond():
 	return render_template("response.html")
 
 def destroy_db(addr):
-	# TODO: helper function to drop a mongodb db at a given IP
 	print("Destroying database at " + addr)
 	client = MongoClient(addr, serverSelectionTimeoutMS=3000)
 	client.drop_database(DBNAME)
 
 def create_db(addr):
-	if destroy_db(addr) == False:
-		return False
+	client = MongoClient(addr, serverSelectionTimeoutMS=3000)
+	collection = client[DBNAME][COLNAME]
 	print("Creating database at " + addr)
-	# with open('senator.json', 'r') as f:
-		# pprint.pprint(json_util.loads(f.read()))
-	# return False
+	with open('senator.json', 'r') as f:
+		collection.insert_many(json.loads(f.read())['objects'])
 
 if __name__ == "__main__":
+	destroy_db(DEFAULTADDR)
 	create_db(DEFAULTADDR)
 	app.debug = True
-	app.run()
+	# use_reloader prevents create_db from executing twice
+	app.run(use_reloader=False)
