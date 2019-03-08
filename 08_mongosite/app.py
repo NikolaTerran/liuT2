5f61a385cf41ed92237dacb60c2e645be7b6f31b
Brian Lee <hlee5@stuy.edu>
2019-03-07 21:42:29 -0500

    structure change

--- 08_mongosite/app.py
+++ 08_mongosite/app.py
@@ -11,56 +11,49 @@ then I just used mongoimport with --jsonArray attached to it
 """
 
 import os
-from collections import OrderedDict
-from flask import Flask, render_template, request, redirect, url_for, session
-#from flask_pymongo import PyMongo
 import pprint
+
+from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
 from bson import json_util
-
-import signal
-import sys
-
-#addr = "google.com"
-
-#conf
-#addr = 'mud.ddns.net:27017'
-#db = 'gov'
-#collection = 'senators'
-#json_file = 'senator.json'
-#endconf
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
-class DataStore():
-	collection = None
-
-data = DataStore()
+# mongodb constants
+DEFAULTADDR = "mud.ddns.net"
+DBNAME = "gov"
+COLNAME = "senators"
 
 @app.route('/', methods=['GET','POST'])
 def main():
 	if request.method == 'POST':
 		addr = request.form['ip']
-		db = request.form['db']
-		collection = request.form['collection']
-		client = MongoClient(addr,document_class=OrderedDict)
+		if len(addr) == 0:
+			addr = DEFAULTADDR
+		client = MongoClient(addr)
 
-
-		#my db and collection name
-		db = client[db]
-		collection = db[collection]
-		data.collection = collection 
-		print(collection)
+		db = client[DBNAME]
+		collection = db[COLNAME]
 		session['addr'] = addr
-		#print(type(collection))
-		#json_util.dumps(collection)
-		#print(type(collection))
-		#session['collection'] = json_util.dumps(collection,default=json_util.default)
-		##########################
 		return render_template("request.html")
 	else:
 		return render_template("home.html")
+
+@app.route('/config', methods=['GET','POST'])
+def config():
+	if request.method == 'POST':
+		addr = request.form['ip']
+		if len(addr) == 0:
+			addr = DEFAULTADDR
+		client = MongoClient(addr)
+
+		db = client[DBNAME]
+		collection = db[COLNAME]
+		session['addr'] = addr
+		return render_template("config.html")
+	else:
+		return render_template("config.html")
 
 @app.route('/connect',methods=['GET','POST'])
 def connectdb():
@@ -71,14 +64,7 @@ def connectdb():
 			list = []
 			for senator in data.collection.find({"person.firstname":txt}):
 				pprint.pprint(senator)
-	#json_util.dups(collection)
-	#session['collection'] = collection
-			#print(result)
-			#	pprint.pprint(post)
-		#print(list)	
 		return render_template("respond.html")
-	#else:
-	#	return redirect(url_for('connectdb'))
 
 @app.route('/respond')
 def respond():
@@ -86,103 +72,14 @@ def respond():
 		#addr = request.form[]
 	return render_template("response.html")
 
+def destroy_db():
+	# TODO: helper function to drop a mongodb db at a given IP
+	pass
 
+def create_db():
+	# TODO: helper function to create a mongodb db at a given IP
+	pass
 
-
-"""
-fd = open(json_file,'r')
-line = fd.readline()
-counter = 1
-while line:
-	#print(line)
-	line = fd.readline()
-	data = json_util.loads(line)
-	doc = db.doc
-	doc_id = doc.insert_one(data).inserted_id
-	counter += 1
-
-fd.close()
-
-
-#pprint.pprint(doc.find_one())
-
-def one():
-	#print("ok\n")
-	print("first letter of your response should be capitalized")
-	txt = raw_input("enter the firstname\n")
-	for post in collection.find({"person.firstname":txt}):
-		pprint.pprint(post)
-	#{"address": {"zipcode": txt}}))
-
-def two():
-	print("first letter of your response should be capitalized")
-	txt = raw_input("enter the lastname\n")
-	for post in collection.find({"person.lastname":txt}):
-		pprint.pprint(post)
-
-def three():
-	#txt = raw_input("enter the cspnid\n")
-	xtx = input("enter the cspanid\n")
-	for post in collection.find({"person.cspanid":xtx}):
-		pprint.pprint(post)
-
-def four():
-	print("first letter of your response should be capitalized, and don't append \"party\" to it")
-	txt = raw_input("enter the party name\n")
-	#xtx = input("enter the score\n")
-	for post in collection.find({"party":txt}):
-		pprint.pprint(post)
-	#	pprint.pprint(post)
-	#print("donno how to do it, need more time reading the pymongo doc\n")
-
-
-def five():
-	print("bye\n")
-	sys.exit(0)
-
-
-while True:
-	print("Pymongo demo program")
-	print("Using mongodb at " + addr)
-	print("Please make sure your pymongo version <= 3.4.0")
-	print("choose one")
-	print("1. find senator/s by firstname")
-	print("2. find senator/s by lastname")
-	print("3. find senator by cspanid")
-	print("4. find senator by party")
-	print("other: exit\n")
-	txt = input("waiting for a numerical input\n")
-	
-	if(txt == 1):
-		one()
-	elif(txt == 2):
-		two()
-	elif(txt == 3):
-		three()
-	elif(txt == 4):
-		four()
-	else:
-		five()
-"""
-
-#if necessary, comment out the code below and uncomment
-#code above to test db functionality
 if __name__ == "__main__":
 	app.debug = True
 	app.run()
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
