2f6fb2a5ba1ea98ed8ae309293111b603a3b14e6
Nikola Terran <nikolaterranthe1@gmail.com>
2019-04-12 12:33:49 -0400

    s

---
+++ 17/app.py
@@ -0,0 +1,43 @@
+def ls_function(list):
+	#x = input("type input")
+	result = [x for x in list if int(x) % 22 == 0]
+	print(result)
+	return 0
+
+this_list = ['88','22','33','44','55','66','00']
+list2 = [1,3,7,2,4,0,0,0,1,4,56,17,28,27,37,47]
+
+def function2(list):
+	result = [x for x in list if x % 10 == 7]
+	print(result)
+	return 0
+
+def function3(list):
+	result = [x for x in list if x == 1 or x == 0 or x == 2 or x == 4]
+	print(result)
+	return 0
+
+def function4():
+	result = range(101)
+	result = [x for x in result if x != 2 or x != 3 or x != 1 or x != 5 or x != 7 or  x % 2 == 0 or x % 3 == 0 or x % 5 == 0 or x % 7 == 0]
+	print(result)
+	return 0
+
+def function5():
+	result = range(101)
+	result = [x for x in result if ((x % 2 != 0 and x % 3 != 0) and (x % 5 != 0 and x % 7 != 0)) or x == 2 or x == 3 or x == 5 or x == 7]
+	result = [x for x in result if x != 1]
+	print(result)
+	return 0
+
+def function6(num):
+	result = [x for x in range(num + 1) (for y in range(num + 1) if num % y == 0]
+	print(result)
+	
+ls_function(this_list)
+
+function2(list2)
+function3(list2)
+function4()
+function5()
+function6(100)
