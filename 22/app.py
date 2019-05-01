124668b671c89d3b23e60ea965c5da2591acc043
nikolaterran <nikolaterranthe1@gmail.com>
2019-05-01 04:06:59 -0400

    S

--- 22/app.py
+++ 22/app.py
@@ -1,10 +1,25 @@
-def make_counter():
-    x = 0
-    def subfx():
-        nonlocal x
-        x += 1
-        print(x)
+print("repeat")
+def repeat(x):
+    def subfx(y):
+        print(x * y)
     return subfx
+
+r1 = repeat("hello")
+r1(2)
+r2 = repeat("goodbye")
+r2(2)
+repeat("cool")(3)
+
+
+print("make counter")
+def make_counter():
+    def subfx():
+        #nonlocal x
+        subfx.x += 1
+        print(subfx.x)
+    subfx.x = 0
+    return subfx
+
 
 ctr1 = make_counter()
 
@@ -18,4 +33,11 @@ ctr2()
 ctr1()
 
 ctr2()
-    
+
+print("accessor")
+
+def accessor(x):
+    print(x.x)
+
+accessor(ctr1)
+accessor(ctr2)
